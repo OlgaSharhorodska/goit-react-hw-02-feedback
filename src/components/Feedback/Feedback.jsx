7b88a1cbc31d0cea389a8c7c301f5c6d52191128
OlgaSharhorodska <olga.sharhorodska@gmail.com>
2023-10-13 01:33:32 +0300

    add component+logics

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,27 @@
+export default function FeedbackOptions({ onClickButtons }) {
+  return (
+    <div className="btn-wrapper">
+      <button
+        className="btn btn-good"
+        type="button"
+        onClick={e => onClickButtons(e.target.textContent)}
+      >
+        Good
+      </button>
+      <button
+        className="btn btn-neutral"
+        type="button"
+        onClick={e => onClickButtons(e.target.textContent)}
+      >
+        Neutral
+      </button>
+      <button
+        className="btn btn-bad"
+        type="button"
+        onClick={e => onClickButtons(e.target.textContent)}
+      >
+        Bad
+      </button>
+    </div>
+  );
+}
---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,17 @@
+export default function Statistics({ good, bad, neutral, total, percent }) {
+  return (
+    <div className="wrapper">
+      <span>Good: {good}</span>
+      <span>Neutral: {neutral}</span>
+      <span>Bad: {bad}</span>
+      <span>Total: {total}</span>
+      <span>
+        {total === 0 ? (
+          <span>Positive feedback: </span>
+        ) : (
+          <span>Positive feedback: {percent}%</span>
+        )}
+      </span>
+    </div>
+  );
+}
